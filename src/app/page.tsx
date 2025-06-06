f43bfba4d64079b5df8b22c2511f91294c776585
Jeremy Swinnen <hey@jeremys.be>
2025-06-06 12:51:35 +0200

    Added 'headerImage' to 'page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@ export const dynamic = "force-dynamic";
 
 import { getPageMetadata } from "@/lib/getPageMetadata";
 import { client } from "@/sanity/client";
+import { urlFor } from "@/sanity/imageUrl";
 import { PortableText } from "@portabletext/react";
+import Image from "next/image";
 
 const PAGE_QUERY = `*[
   _type == "page" && slug.current == "home"
-][0]{_id, title, body}`;
+][0]{_id, title, body, headerImage}`;
 
 export async function generateMetadata() {
   return getPageMetadata("home");
@@ -25,6 +27,9 @@ export default async function Home() {
 
   return (
     <main className="container mx-auto min-h-screen max-w-3xl p-8">
+      {page.headerImage && (
+        <img src={urlFor(page.headerImage).url()} alt={page.headerImage.alt} />
+      )}
       <h1 className="text-4xl font-bold mb-8">{page.title}</h1>
       <PortableText value={page.body} />
     </main>
