035b50ab01fec4efc2b9e931ec76e7973cb98575
Jeremy Swinnen <hey@jeremys.be>
2025-06-20 16:12:31 +0200

    Added 'about' and 'contact' pages

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,41 @@
+export const dynamic = "force-dynamic";
+
+import { getPageMetadata } from "@/lib/getPageMetadata";
+import { client } from "@/sanity/client";
+import { urlFor } from "@/sanity/imageUrl";
+import { PortableText } from "@portabletext/react";
+
+const PAGE_QUERY = `*[
+  _type == "page" && slug.current == "contact"
+][0]{_id, title, body, headerImage}`;
+
+export async function generateMetadata() {
+  return getPageMetadata("contact");
+}
+
+export default async function Home() {
+  const page = await client.fetch(PAGE_QUERY);
+
+  if (!page) {
+    return (
+      <section>
+        <h1 className="text-4xl font-bold">Contact page not found</h1>
+      </section>
+    );
+  }
+
+  return (
+    <section className="col-span-full grid grid-cols-subgrid">
+      {page.headerImage && (
+        <img
+          className="col-span-3"
+          src={urlFor(page.headerImage).url()}
+          alt={page.headerImage.alt}
+        />
+      )}
+      <article className="col-span-5">
+        <PortableText value={page.body} />
+      </article>
+    </section>
+  );
+}
