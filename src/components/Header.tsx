28d0ac3776c8295a1cb22e2e414c24612719f27e
Jeremy Swinnen <hey@jeremys.be>
2025-06-21 14:58:03 +0200

    Implemented a submenu inside the 'Header' component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,12 +12,23 @@ type Navigation = {
 
 const NAV_QUERY = `*[_type == "navigation"][0]{links}`;
 
+const SOLUTIONS_QUERY = `*[
+  _type == "solution"
+] | order(name asc) {
+  _id,
+  name,
+  slug
+}`;
+
 type HeaderProps = {
   className?: string;
 };
 
 export default async function Header({ className }: HeaderProps = {}) {
-  const nav: Navigation = await client.fetch(NAV_QUERY);
+  const [nav, solutions] = await Promise.all([
+    client.fetch(NAV_QUERY),
+    client.fetch(SOLUTIONS_QUERY),
+  ]);
 
   return (
     <header
@@ -30,11 +41,32 @@ export default async function Header({ className }: HeaderProps = {}) {
       </div>
       <nav>
         <ul className="flex gap-6">
-          {(nav?.links || []).map((link: NavLink) => (
-            <li key={link.slug}>
-              <Link href={`/${link.slug}`}>{link.title}</Link>
-            </li>
-          ))}
+          {(nav?.links || []).map((link: NavLink) => {
+            if (link.title === "Oplossingen") {
+              return (
+                <li key={link.slug} className="relative group">
+                  <Link href={`/${link.slug}`}>{link.title}</Link>
+                  <ul className="absolute left-0 top-full w-56 bg-white shadow-lg rounded hidden group-hover:block z-10 border border-gray-200">
+                    {solutions.map((solution: any) => (
+                      <li key={solution._id}>
+                        <Link
+                          href={`/oplossingen/${solution.slug.current}`}
+                          className="block px-4 py-2 hover:bg-gray-100 whitespace-nowrap"
+                        >
+                          {solution.name}
+                        </Link>
+                      </li>
+                    ))}
+                  </ul>
+                </li>
+              );
+            }
+            return (
+              <li key={link.slug}>
+                <Link href={`/${link.slug}`}>{link.title}</Link>
+              </li>
+            );
+          })}
         </ul>
       </nav>
     </header>
