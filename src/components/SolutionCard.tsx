855c9714c9c402db5f1e53afafbd8aafe344a734
Jeremy Swinnen <hey@jeremys.be>
2025-07-04 11:08:29 +0200

    Tweaked 'SolutionCard' layout and hover effect

--- src/components/SolutionCard.tsx
+++ src/components/SolutionCard.tsx
@@ -15,14 +15,14 @@ export default function SolutionCard({ solution }: SolutionCardProps) {
     <Link
       href={`/oplossingen/${solution.slug.current}`}
       key={solution._id}
-      className="group bg-white p-3 rounded-xl shadow-md flex flex-col items-center gap-3 overflow-hidden transition-transform duration-200 hover:scale-[1.025]"
+      className="group bg-white p-3 rounded-2xl shadow-sm flex flex-col items-center gap-3 overflow-hidden transition-all duration-300 hover:scale-[1.025] hover:shadow-md"
     >
       {solution.headerImage && (
-        <div className="aspect-w-16 aspect-h-9">
+        <div className="w-full overflow-hidden rounded-lg">
           <img
             src={urlFor(solution.headerImage).url()}
             alt={solution.headerImage.alt || solution.name}
-            className="rounded-md w-full h-full object-cover"
+            className="group-hover:scale-105 rounded-lg object-cover max-h-[280px] w-full transition-all duration-700"
           />
         </div>
       )}
