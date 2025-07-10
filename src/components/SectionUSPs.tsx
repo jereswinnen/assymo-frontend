51eb0fed6d78c81ee922220082869fc32a532ec8
Jeremy Swinnen <hey@jeremys.be>
2025-07-10 17:37:32 +0200

    UPSSection mobile tweaks

--- src/components/SectionUSPs.tsx
+++ src/components/SectionUSPs.tsx
@@ -24,19 +24,19 @@ const usps = [
 
 export default function SectionUSPs() {
   return (
-    <section className="col-start-2 col-span-6 grid grid-cols-subgrid gap-y-5">
+    <section className="px-container-sm md:px-container-md col-span-full md:col-start-2 md:col-span-6 grid grid-cols-subgrid gap-y-5">
       <header className="col-span-full flex justify-center">
         <h2>Wat maakt Assymo zo goed?</h2>
       </header>
-      <section className="col-span-full grid grid-cols-subgrid items-center">
-        <div className="col-span-2">
+      <section className="col-span-full grid grid-cols-subgrid items-center gap-y-8 md:gap-y-0">
+        <div className="col-span-full md:col-span-2">
           <img
             src="/images/sectionUSPImage.jpg"
             alt="Onze voordelen"
-            className="rounded-xl object-cover"
+            className="max-h-[280px] md:max-h-none w-full md:w-auto rounded-xl object-cover"
           />
         </div>
-        <div className="col-span-4 flex flex-col gap-10">
+        <div className="col-span-full md:col-span-4 flex flex-col gap-10">
           <ul className="flex flex-col gap-10">
             {usps.map((usp, idx) => (
               <li key={idx} className="flex items-start gap-4">
