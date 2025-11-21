d26749afbd63f37a6a1c32562bb612cb8cd5f60b
Jeremy Swinnen <hey@jeremys.be>
2025-11-21 16:12:24 +0100

    Fine-tuned header interactions

--- src/components/HeaderClient.tsx
+++ src/components/HeaderClient.tsx
@@ -13,16 +13,19 @@ import {
 import { Separator } from "./ui/separator";
 import { motion, AnimatePresence } from "motion/react";
 import { useState, useEffect } from "react";
-import { createPortal } from "react-dom";
+import { createPortal, flushSync } from "react-dom";
 import { urlFor } from "@/sanity/imageUrl";
 
 // Animation tokens
 const easing = "circInOut";
+const translateHorizontal = 8;
 const translateVertical = -8;
 const portalDuration = 0.4;
 const subMenuHeightDuration = 0.5;
 const subMenuOpacityDuration = 0.4;
 const subMenuInnerElementDuration = 0.4;
+const subMenuSwitchDuration = 0.4;
+const subMenuBlur = "2px";
 const carouselInterval = 1900;
 const carouselTransitionDuration = 0.4;
 
@@ -63,9 +66,14 @@ export default function HeaderClient({
   className,
 }: HeaderClientProps) {
   const [activeLink, setActiveLink] = useState<NavLink | null>(null);
+  const [activeIndex, setActiveIndex] = useState(-1);
+  const [direction, setDirection] = useState(1);
   const [currentIndex, setCurrentIndex] = useState(0);
   const [isHoveringItem, setIsHoveringItem] = useState(false);
   const [mounted, setMounted] = useState(false);
+  const [animationMode, setAnimationMode] = useState<
+    "open" | "switch" | "close"
+  >("open");
 
   const isSubmenuOpen = activeLink !== null;
 
@@ -86,16 +94,36 @@ export default function HeaderClient({
     return () => clearInterval(interval);
   }, [isSubmenuOpen, isHoveringItem, activeLink]);
 
-  const handleLinkHover = (link: NavLink) => {
+  const handleLinkHover = (link: NavLink, index: number) => {
     if (link.subItems && link.subItems.length > 0) {
+      if (activeIndex !== -1) {
+        setDirection(index > activeIndex ? 1 : -1);
+        setAnimationMode("switch");
+      } else {
+        setAnimationMode("open");
+      }
       setActiveLink(link);
+      setActiveIndex(index);
+      setCurrentIndex(0);
+      setIsHoveringItem(false);
+    } else {
+      // Close submenu when hovering item without subItems
+      flushSync(() => {
+        setAnimationMode("close");
+      });
+      setActiveLink(null);
+      setActiveIndex(-1);
       setCurrentIndex(0);
       setIsHoveringItem(false);
     }
   };
 
   const handleMouseLeave = () => {
+    flushSync(() => {
+      setAnimationMode("close");
+    });
     setActiveLink(null);
+    setActiveIndex(-1);
     setCurrentIndex(0);
     setIsHoveringItem(false);
   };
@@ -144,10 +172,13 @@ export default function HeaderClient({
             <Logo className="w-28" />
           </Link>
 
-          <nav className="text-sm bg-ambr-200">
+          <nav className="text-sm">
             <ul className="flex gap-6 group/nav">
-              {links.map((link) => (
-                <li key={link.slug} onMouseEnter={() => handleLinkHover(link)}>
+              {links.map((link, index) => (
+                <li
+                  key={link.slug}
+                  onMouseEnter={() => handleLinkHover(link, index)}
+                >
                   <Link
                     href={`/${link.slug}`}
                     className="font-medium transition-opacity duration-200 group-hover/nav:opacity-60 hover:!opacity-100"
@@ -178,134 +209,176 @@ export default function HeaderClient({
                 opacity: { duration: subMenuOpacityDuration, ease: easing },
               }}
             >
-              <div className="mx-auto w-full max-w-site flex gap-8 py-8">
-                <motion.figure
-                  className="w-2xs h-96 relative overflow-hidden"
-                  initial={{ opacity: 0, y: translateVertical }}
-                  animate={{ opacity: 1, y: 0 }}
-                  exit={{ opacity: 0, y: translateVertical }}
-                  transition={{
-                    duration: subMenuInnerElementDuration,
-                    ease: easing,
-                  }}
+              {/* Inner content with translateVertical on open/close */}
+              <motion.div
+                className="w-full"
+                initial={{ y: translateVertical, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                exit={{ y: translateVertical, opacity: 0 }}
+                transition={{
+                  duration: subMenuInnerElementDuration,
+                  ease: easing,
+                }}
+              >
+                <AnimatePresence
+                  mode="wait"
+                  custom={{ animationMode, direction }}
                 >
-                  <AnimatePresence>
-                    {currentImage && (
-                      <motion.img
-                        key={currentIndex}
-                        src={urlFor(currentImage).url()}
-                        alt={currentImage.alt || ""}
-                        className="absolute inset-0 w-full h-full object-cover"
-                        initial={{ opacity: 0 }}
-                        animate={{ opacity: 1 }}
-                        exit={{ opacity: 0 }}
-                        transition={{
-                          duration: carouselTransitionDuration,
-                          ease: "easeInOut",
-                        }}
-                      />
-                    )}
-                  </AnimatePresence>
-                </motion.figure>
-                <motion.div
-                  className="flex flex-col gap-3"
-                  initial={{ opacity: 0, y: translateVertical }}
-                  animate={{ opacity: 1, y: 0 }}
-                  exit={{ opacity: 0, y: translateVertical }}
-                  transition={{
-                    duration: subMenuInnerElementDuration,
-                    ease: easing,
-                  }}
-                >
-                  {activeLink?.submenuHeading && (
-                    <span className="text-xs font-medium text-stone-600">
-                      {activeLink.submenuHeading}
-                    </span>
-                  )}
-                  <ul
-                    className="flex flex-col gap-1.5 text-2xl font-semibold [&>*>*]:block [&>*>*]:transition-all [&>*>*]:duration-300 [&>*>*]:hover:translate-x-1.5"
-                    onMouseLeave={handleItemsLeave}
+                  <motion.div
+                    key={activeLink?.slug}
+                    className="mx-auto w-full max-w-site flex gap-8 py-8"
+                    custom={{ animationMode, direction }}
+                    initial="enter"
+                    animate="center"
+                    exit="exit"
+                    variants={{
+                      enter: ({
+                        animationMode,
+                        direction,
+                      }: {
+                        animationMode: string;
+                        direction: number;
+                      }) => ({
+                        opacity: animationMode === "switch" ? 0 : 1,
+                        x:
+                          animationMode === "switch"
+                            ? direction * translateHorizontal
+                            : 0,
+                        filter:
+                          animationMode === "switch"
+                            ? `blur(${subMenuBlur})`
+                            : "blur(0px)",
+                      }),
+                      center: {
+                        opacity: 1,
+                        x: 0,
+                        filter: "blur(0px)",
+                      },
+                      exit: ({
+                        animationMode,
+                        direction,
+                      }: {
+                        animationMode: string;
+                        direction: number;
+                      }) => ({
+                        opacity: animationMode === "switch" ? 0 : 1,
+                        x:
+                          animationMode === "switch"
+                            ? -direction * translateHorizontal
+                            : 0,
+                        filter:
+                          animationMode === "switch"
+                            ? `blur(${subMenuBlur})`
+                            : "blur(0px)",
+                      }),
+                    }}
+                    transition={{
+                      duration: subMenuSwitchDuration,
+                      ease: easing,
+                    }}
                   >
-                    {activeLink?.subItems?.map((item, index) => (
-                      <li
-                        key={item.slug.current}
-                        onMouseEnter={() => handleItemHover(index)}
+                    <figure className="w-2xs h-96 relative overflow-hidden">
+                      <AnimatePresence>
+                        {currentImage && (
+                          <motion.img
+                            key={currentIndex}
+                            src={urlFor(currentImage).url()}
+                            alt={currentImage.alt || ""}
+                            className="absolute inset-0 w-full h-full object-cover"
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                            exit={{ opacity: 0 }}
+                            transition={{
+                              duration: carouselTransitionDuration,
+                              ease: "easeInOut",
+                            }}
+                          />
+                        )}
+                      </AnimatePresence>
+                    </figure>
+                    <div className="flex flex-col gap-3">
+                      {activeLink?.submenuHeading && (
+                        <span className="text-xs font-medium text-stone-600">
+                          {activeLink.submenuHeading}
+                        </span>
+                      )}
+                      <ul
+                        className="flex flex-col gap-1.5 text-2xl font-semibold [&>*>*]:block [&>*>*]:transition-all [&>*>*]:duration-300 [&>*>*]:hover:translate-x-1.5"
+                        onMouseLeave={handleItemsLeave}
                       >
-                        <Link href={`/oplossingen/${item.slug.current}`}>
-                          {item.name}
-                        </Link>
-                      </li>
-                    ))}
-                  </ul>
-                </motion.div>
-                <motion.div
-                  className="ml-auto w-3xs flex flex-col gap-3"
-                  initial={{ opacity: 0, y: translateVertical }}
-                  animate={{ opacity: 1, y: 0 }}
-                  exit={{ opacity: 0, y: translateVertical }}
-                  transition={{
-                    duration: subMenuInnerElementDuration,
-                    ease: easing,
-                  }}
-                >
-                  <span className="text-xs font-medium text-stone-600">
-                    Contacteer
-                  </span>
-                  <div className="flex flex-col gap-6">
-                    <ul className="flex flex-col gap-3 text-base font-medium">
-                      {settings?.address && (
-                        <li className="whitespace-pre-line">
-                          {settings.address}
-                        </li>
-                      )}
-                      {settings?.phone && (
-                        <li>
-                          <a
-                            href={`tel:${settings.phone}`}
-                            className="flex items-center gap-2 text-stone-500 hover:text-stone-700 transition-colors duration-300"
+                        {activeLink?.subItems?.map((item, index) => (
+                          <li
+                            key={item.slug.current}
+                            onMouseEnter={() => handleItemHover(index)}
                           >
-                            <PhoneIcon className="size-4" />
-                            <span>{settings.phone}</span>
-                          </a>
-                        </li>
-                      )}
-                    </ul>
-                    {(settings?.instagram || settings?.facebook) && (
-                      <>
-                        <Separator />
-                        <ul className="flex flex-col gap-3 text-sm font-medium">
-                          {settings?.instagram && (
-                            <li>
-                              <a
-                                className="flex items-center gap-2 text-stone-500 hover:text-stone-700 transition-colors duration-300"
-                                href={settings.instagram}
-                                target="_blank"
-                                rel="noopener noreferrer"
-                              >
-                                <InstagramIcon className="size-4" />
-                                Instagram
-                              </a>
+                            <Link href={`/oplossingen/${item.slug.current}`}>
+                              {item.name}
+                            </Link>
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
+                    <div className="ml-auto w-3xs flex flex-col gap-3">
+                      <span className="text-xs font-medium text-stone-600">
+                        Contacteer
+                      </span>
+                      <div className="flex flex-col gap-6">
+                        <ul className="flex flex-col gap-3 text-base font-medium">
+                          {settings?.address && (
+                            <li className="whitespace-pre-line">
+                              {settings.address}
                             </li>
                           )}
-                          {settings?.facebook && (
+                          {settings?.phone && (
                             <li>
                               <a
+                                href={`tel:${settings.phone}`}
                                 className="flex items-center gap-2 text-stone-500 hover:text-stone-700 transition-colors duration-300"
-                                href={settings.facebook}
-                                target="_blank"
-                                rel="noopener noreferrer"
                               >
-                                <FacebookIcon className="size-4" />
-                                Facebook
+                                <PhoneIcon className="size-4" />
+                                <span>{settings.phone}</span>
                               </a>
                             </li>
                           )}
                         </ul>
-                      </>
-                    )}
-                  </div>
-                </motion.div>
-              </div>
+                        {(settings?.instagram || settings?.facebook) && (
+                          <>
+                            <Separator />
+                            <ul className="flex flex-col gap-3 text-sm font-medium">
+                              {settings?.instagram && (
+                                <li>
+                                  <a
+                                    className="flex items-center gap-2 text-stone-500 hover:text-stone-700 transition-colors duration-300"
+                                    href={settings.instagram}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                  >
+                                    <InstagramIcon className="size-4" />
+                                    Instagram
+                                  </a>
+                                </li>
+                              )}
+                              {settings?.facebook && (
+                                <li>
+                                  <a
+                                    className="flex items-center gap-2 text-stone-500 hover:text-stone-700 transition-colors duration-300"
+                                    href={settings.facebook}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                  >
+                                    <FacebookIcon className="size-4" />
+                                    Facebook
+                                  </a>
+                                </li>
+                              )}
+                            </ul>
+                          </>
+                        )}
+                      </div>
+                    </div>
+                  </motion.div>
+                </AnimatePresence>
+              </motion.div>
             </motion.div>
           )}
         </AnimatePresence>
