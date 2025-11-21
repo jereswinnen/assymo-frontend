99eac0c5668758c10e12758f5a4c0af8bb1a4363
Jeremy Swinnen <hey@jeremys.be>
2025-11-21 11:56:24 +0100

    Updated header structure and added Motion package

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -4,8 +4,6 @@ import { getPageMetadata } from "@/lib/getPageMetadata";
 import { client } from "@/sanity/client";
 import { sectionsFragment } from "@/sanity/fragments";
 import SectionRenderer from "@/components/SectionRenderer";
-import { Separator } from "@/components/ui/separator";
-import { ListTreeIcon, PhoneIcon } from "lucide-react";
 
 const PAGE_QUERY = `*[
   _type == "page" && slug.current == "home"
@@ -34,58 +32,6 @@ export default async function HomePage() {
 
   return (
     <>
-      <div className="col-span-full flex gap-8 bg-amber-200">
-        <figure className="w-3xs">
-          <img
-            src="https://images.unsplash.com/photo-1599696848652-f0ff23bc911f?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt=""
-          />
-        </figure>
-        <div className="flex flex-col gap-3">
-          <span className="text-xs font-medium text-stone-600">Ontdek</span>
-          <ul className="flex flex-col gap-1 text-2xl font-semibold [&>*>*]:block [&>*>*]:transition-all [&>*>*]:duration-300 [&>*>*]:hover:translate-x-2">
-            <li>
-              <a href="#">Trappen</a>
-            </li>
-            <li>
-              <a href="#">Badkamer</a>
-            </li>
-            <li>
-              <a href="#">Keuken</a>
-            </li>
-            <li>
-              <a href="#">Bureau</a>
-            </li>
-            <li>
-              <a href="#">Meubilair</a>
-            </li>
-          </ul>
-        </div>
-        <div className="ml-auto flex flex-col gap-2">
-          <span className="text-xs font-medium text-stone-600">Contacteer</span>
-          <div className="flex flex-col gap-4">
-            <ul className="flex flex-col gap-2 text-base font-medium">
-              <li>
-                Eikenlei 159,<br></br>2960 Sint-Job-in-&apos;t-Goor
-              </li>
-              <li className="flex items-center gap-2">
-                <PhoneIcon className="size-4" />
-                <a href="#">+32 (0) 3 434 74 98</a>
-              </li>
-            </ul>
-            <Separator />
-            <ul className="flex flex-col gap-1 text-sm font-medium">
-              <li>
-                <a href="#">Instagram</a>
-              </li>
-              <li>
-                <a href="#">Facebook</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-
       {page.sections && page.sections.length > 0 && (
         <SectionRenderer sections={page.sections} />
       )}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,13 @@ import Logo from "./Logo";
 import NavLinks from "./NavLinks";
 import { cn } from "@/lib/utils";
 import { Action } from "./Action";
-import { Calendar1Icon } from "lucide-react";
+import {
+  Calendar1Icon,
+  FacebookIcon,
+  InstagramIcon,
+  PhoneIcon,
+} from "lucide-react";
+import { Separator } from "./ui/separator";
 
 const NAV_QUERY = `*[_type == "navigation"][0]{links}`;
 
@@ -27,23 +33,95 @@ export default async function Header({ className }: HeaderProps) {
   ]);
 
   return (
-    <header className={cn("md:sticky md:top-0 z-50 py-8", className)}>
-      <div className="mx-auto max-w-site flex flex-col md:flex-row md:items-center justify-start md:justify-between gap-8">
+    <header
+      className={cn(
+        "md:flex md:flex-col md:gap-16 md:sticky md:top-0 z-50 py-8 bg-amber-200",
+        className,
+      )}
+    >
+      <div className="mx-auto w-full max-w-site flex flex-col md:flex-row md:items-center justify-start md:justify-between gap-8">
         <Link href="/">
           <Logo className="w-28" />
         </Link>
 
-        <div className="flex flex-auto items-center justify-center">
-          <nav className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-sm bg-amber-200">
-            <NavLinks links={nav?.links || []} solutions={solutions} />
-          </nav>
-        </div>
+        <nav className="text-sm bg-ambr-200">
+          <NavLinks links={nav?.links || []} solutions={solutions} />
+        </nav>
 
         <Action
           href="/contact"
           icon={<Calendar1Icon />}
           label="Maak een afspraak"
         />
+      </div>
+
+      <div className="mx-auto w-full max-w-site flex gap-8 bg-pink-200">
+        <figure className="w-2xs">
+          <img
+            src="https://images.unsplash.com/photo-1599696848652-f0ff23bc911f?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+          />
+        </figure>
+        <div className="flex flex-col gap-3">
+          <span className="text-xs font-medium text-stone-600">Ontdek</span>
+          <ul className="flex flex-col gap-1.5 text-2xl font-semibold [&>*>*]:block [&>*>*]:transition-all [&>*>*]:duration-300 [&>*>*]:hover:translate-x-1.5">
+            <li>
+              <a href="#">Trappen</a>
+            </li>
+            <li>
+              <a href="#">Badkamer</a>
+            </li>
+            <li>
+              <a href="#">Keuken</a>
+            </li>
+            <li>
+              <a href="#">Bureau</a>
+            </li>
+            <li>
+              <a href="#">Meubilair</a>
+            </li>
+          </ul>
+        </div>
+        <div className="ml-auto w-3xs flex flex-col gap-3">
+          <span className="text-xs font-medium text-stone-600">Contacteer</span>
+          <div className="flex flex-col gap-6">
+            <ul className="flex flex-col gap-3 text-base font-medium">
+              <li>
+                Eikenlei 159,<br></br>2960 Sint-Job-in-&apos;t-Goor
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="flex items-center gap-2 text-stone-500 hover:text-stone-700 transition-colors duration-300"
+                >
+                  <PhoneIcon className="size-4" />
+                  <span>+32 (0) 3 434 74 98</span>
+                </a>
+              </li>
+            </ul>
+            <Separator />
+            <ul className="flex flex-col gap-3 text-sm font-medium">
+              <li>
+                <a
+                  className="flex items-center gap-2 text-stone-500 hover:text-stone-700 transition-colors duration-300"
+                  href="#"
+                >
+                  <InstagramIcon className="size-4" />
+                  Instagram
+                </a>
+              </li>
+              <li>
+                <a
+                  className="flex items-center gap-2 text-stone-500 hover:text-stone-700 transition-colors duration-300"
+                  href="#"
+                >
+                  <FacebookIcon className="size-4" />
+                  Facebook
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </header>
   );
