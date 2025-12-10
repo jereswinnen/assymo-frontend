b5d0a53ce266224dded6fa1b46bbdb43b6434c33
Jeremy Swinnen <hey@jeremys.be>
2025-12-10 14:18:23 +0100

    Added Vercel Speed Insights

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -1,3 +1,4 @@
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import ChatbotWrapper from "@/components/ChatbotWrapper";
@@ -21,6 +22,7 @@ export default function SiteLayout({
         <ChatbotWrapper />
         <CookieBanner />
       </div>
+      <SpeedInsights />
     </div>
   );
 }
